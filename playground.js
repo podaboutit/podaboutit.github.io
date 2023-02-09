ec5cb815d3457e8cc5e5cd5a6456ed0b68a5fd5f
podaboutit <122939261+podaboutit@users.noreply.github.com>
2023-02-09 17:57:43 -0600

    updates

---
+++ playground.js
@@ -0,0 +1,6 @@
+// Declare variables (var) and assign them values in the same line.
+var myNickname = "Doug Funny";
+var myName = myNickname;
+// Use let to assign items that won't change
+let myStatus = " is awesome!";
+console.log (myName + myStatus)
